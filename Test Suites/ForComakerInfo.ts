f667f1b66b8aed1b22c3eac3bf16ff0c8c977c6d
Neil <nrochotorena@student.apc.edu.ph>
2021-01-19 21:52:43 +0800

    initial commit from katalon

---
+++ Test Suites/ForComakerInfo.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ForComakerInfo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>28529ca9-c7f5-4dd5-b843-62de92d16a96</testSuiteGuid>
+   <testCaseLink>
+      <guid>20ba1491-0142-491c-9c62-432058746ec0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Common/EnterComakerInfo</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f2012bf7-6ae5-49ea-b7f7-169a667970b9</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/ComakerInfo</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>f2012bf7-6ae5-49ea-b7f7-169a667970b9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>First Name</value>
+         <variableId>5335d10e-4abf-4f6f-bd3d-8a6da17af1f7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f2012bf7-6ae5-49ea-b7f7-169a667970b9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Last Name</value>
+         <variableId>313c1911-d16a-4bf9-9adb-675b0a4eb756</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f2012bf7-6ae5-49ea-b7f7-169a667970b9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Email</value>
+         <variableId>fd98aaa0-3f9a-483c-b80e-dcdb63c5beed</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
